0e83189e4f1738ac698e952e5f03e0f4d528cdc8
Alex Van Vleet <alexvan73@yahoo.com>
2023-10-14 21:28:52 -0600

    testing

--- app/contact/page.js
+++ app/contact/page.js
@@ -18,17 +18,10 @@ export default function Contact() {
     phone: "",
   });
 
-  console.log(process.env);
-
-  const { register, handleSubmit } = useForm();
+  const { handleSubmit } = useForm();
 
   const onSubmit = () => {
-    send(
-      process.env.NEXT_PUBLIC_SERVICE_ID,
-      process.env.NEXT_PUBLIC_TEMPLATE_ID,
-      toSend,
-      process.env.NEXT_PUBLIC_USER_ID
-    )
+    send(service_rxsmi4b, template_wylxjes, toSend, GSu7c8HKQQ1CYrr6c)
       .then((response) => {
         console.log("SUCCESS!", response.status, response.text);
       })
