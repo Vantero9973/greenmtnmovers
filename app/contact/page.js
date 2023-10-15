4669a8dfd21618e6da6c8f9d4551aca79f52d988
Alex Van Vleet <alexvan73@yahoo.com>
2023-10-15 16:47:22 -0600

    testing

--- app/contact/page.js
+++ app/contact/page.js
@@ -20,16 +20,12 @@ export default function Contact() {
 
   const { handleSubmit } = useForm();
 
-  const NEXT_PUBLIC_SERVICE_ID = process.env.NEXT_PUBLIC_SERVICE_ID;
-  const NEXT_PUBLIC_TEMPLATE_ID = process.env.NEXT_PUBLIC_TEMPLATE_ID;
-  const NEXT_PUBLIC_USER_ID = process.env.NEXT_PUBLIC_USER_ID;
-
   const onSubmit = () => {
     send(
-      NEXT_PUBLIC_SERVICE_ID,
-      NEXT_PUBLIC_TEMPLATE_ID,
+      process.env.NEXT_PUBLIC_SERVICE_ID,
+      process.env.NEXT_PUBLIC_TEMPLATE_ID,
       toSend,
-      NEXT_PUBLIC_USER_ID
+      process.env.NEXT_PUBLIC_USER_ID
     )
       .then((response) => {
         console.log("SUCCESS!", response.status, response.text);
