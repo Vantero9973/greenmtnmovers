1db7f2735a803929bcae2e6e2f0a678b080a605e
Alex Van Vleet <vantero.dev@gmail.com>
2024-09-11 23:55:59 +0900

    Add google analytics tag

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-T6XSHG53ZC"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-T6XSHG53ZC');
+            `,
+          }}
+        />
+      </head>
       <body className={inter.className}>
         {children}
         <FooterComponent />
