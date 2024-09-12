eb43de27637082aa396682836ace285203986a32
Alex Van Vleet <vantero.dev@gmail.com>
2024-09-12 16:51:13 +0900

    Fix google analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 import FooterComponent from "../components/FooterComponent";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,10 +15,22 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-T6XSHG53ZC"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-T6XSHG53ZC');
+          `}
+        </Script>
         {children}
         <FooterComponent />
       </body>
-      <GoogleAnalytics gaId="G-T6XSHG53ZC" />
     </html>
   );
 }
