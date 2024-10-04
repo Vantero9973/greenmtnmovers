91564eb04014800ee50bc6ec0090b7d64ecdc05e
Alex Van Vleet <vantero.dev@gmail.com>
2024-10-04 16:09:25 +0900

    Add Google tags

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WZ5MZBLH');
+          `}
+        </Script>
+
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-T6XSHG53ZC"
           strategy="afterInteractive"
@@ -28,6 +38,16 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-T6XSHG53ZC');
           `}
         </Script>
+
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WZ5MZBLH"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         {children}
         <FooterComponent />
       </body>
